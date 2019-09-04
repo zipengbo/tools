dec672f291d885f46d009fdbe12892a0d0c46d61
pengbo.zi <18839972815@126.com>
2019-09-04 14:51:08 +0800

    first commit

---
+++ src/main/java/com/relx/tools/bean/User.java
@@ -0,0 +1,82 @@
+package com.relx.tools.bean;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sang on 2017/12/17.
+ */
+public class User  {
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+    private boolean enabled;
+    private String email;
+    private String userface;
+    private Timestamp regTime;
+
+    public Timestamp getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(Timestamp regTime) {
+        this.regTime = regTime;
+    }
+
+    public String getUserface() {
+        return userface;
+    }
+
+    public void setUserface(String userface) {
+        this.userface = userface;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
